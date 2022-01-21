5f562a16d5e465323bc55bd90c1f3f1c8ad904b8
MisterMAX789 <93163193+MisterMAX789@users.noreply.github.com>
2022-01-21 13:45:35 +0300

    add new folder

---
+++ src/main/java/ru/netology/manager/PostManger.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+import ru.netology.domain.PostSource;
+public class PostManger {
+    private PostSource[] postsource;
+
+    public PostSource [] search (int ownerId, int domain, int query, boolean ownersOnly, int count,int offset){
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+    }
+}
+
